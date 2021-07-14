26a8f88cf1cfd1a43381648a275a85c3ef147114
Mohammed Abdel Raouf <mohammed@raouf.me>
2021-07-14 11:23:02 +0200

    [ZSH] add default-packages list to nvm

--- zsh/plugins/nvm.zsh
+++ zsh/plugins/nvm.zsh
@@ -2,11 +2,12 @@
 #"""                                 NVM                                      """
 #""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 export NVM_AUTO_USE=true
-export NVM_LAZY_LOAD=true
 export NVM_SYMLINK_CURRENT=true
 export NVM_DIR="$XDG_CONFIG_HOME/nvm"
-export NVM_LAZY_LOAD_EXTRA_COMMANDS=("cd" "vim")
+[[ -e "${NVM_DIR}/default-packages" ]] || cat >> "${NVM_DIR}/default-packages" << EOM
+pm2
+yarn
+EOM
 
-zinit ice atload"_zsh-auto-lazy-nvm" nocd silent 
 zinit light lukechilds/zsh-nvm
 #""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
