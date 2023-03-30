0410c6260ca11e959c77796fbcda90bb4cc3c09c
Raouf <mohammed@raouf.me>
2023-03-30 14:29:46 +0200

    [GT-KING] adjust configuration to work on armbian

--- zsh/aliases/shapa.zsh
+++
@@ -1,9 +0,0 @@
-##"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""##
-#""                                   Shapa                                   ""#
-##"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""##
-alias shapadb="docker container start shapadb"
-alias shapadb-stop="docker container stop shapadb"
-alias shapadb-refresh="docker container rm -f shapadb; docker run --name shapadb --publish 5432:5432 --volume $HOME/.work/Shapa/shapa-server/tmp/database_backups/:/backups/ --env POSTGRES_PASSWORD=password --detach postgres:11-alpine"
-alias shapadb-shell="docker exec -it shapadb bash"
-alias shapadb-db-shell="docker exec -it shapadb psql -U postgres"
-##"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""##
