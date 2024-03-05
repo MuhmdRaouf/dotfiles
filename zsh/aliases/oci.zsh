da2f19232eb2117a6f0bb98997490ec8ab03bd31
Raouf <mohammed@raouf.me>
2024-03-05 15:59:56 +0200

    [OCI] add oracle cloud infrastructure configs

---
+++ zsh/aliases/oci.zsh
@@ -0,0 +1,6 @@
+##"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""##
+#""                                   OCI                                     ""#
+##"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""##
+alias oci-eu="oci --config-file /Users/kaizen/.oci/config --profile EU --auth security_token"
+alias oci-us="oci --config-file /Users/kaizen/.oci/config --profile US --auth security_token"
+##"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""##
