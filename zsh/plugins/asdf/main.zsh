589d833600cb9ca359e19a344eb4662de70b303a
Raouf <mohammed@raouf.me>
2024-08-02 18:37:05 +0300

    [ZSH] Update zsh paths with predefined vars

--- zsh/plugins/asdf/main.zsh
+++ zsh/plugins/asdf/main.zsh
@@ -1,18 +1,18 @@
 #""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""##
 #""                                   nasdf                                   ""#
 #""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""##
-source $XDG_CONFIG_HOME/zsh/plugins/asdf/asdf.zsh
+source $XDG_ZSH_HOME/plugins/asdf/asdf.zsh
 #""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 ##"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""##
 #""                                asdf plugins                               ""#
 ##"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""##
-source $XDG_CONFIG_HOME/zsh/plugins/asdf/plugins/direnv.zsh
-source $XDG_CONFIG_HOME/zsh/plugins/asdf/plugins/golang.zsh
-source $XDG_CONFIG_HOME/zsh/plugins/asdf/plugins/java.zsh
-source $XDG_CONFIG_HOME/zsh/plugins/asdf/plugins/nodejs.zsh
-source $XDG_CONFIG_HOME/zsh/plugins/asdf/plugins/perl.zsh
-source $XDG_CONFIG_HOME/zsh/plugins/asdf/plugins/python.zsh
-source $XDG_CONFIG_HOME/zsh/plugins/asdf/plugins/ruby.zsh
-source $XDG_CONFIG_HOME/zsh/plugins/asdf/plugins/rust.zsh
+source $XDG_ZSH_HOME/plugins/asdf/plugins/direnv.zsh
+source $XDG_ZSH_HOME/plugins/asdf/plugins/golang.zsh
+source $XDG_ZSH_HOME/plugins/asdf/plugins/java.zsh
+source $XDG_ZSH_HOME/plugins/asdf/plugins/nodejs.zsh
+source $XDG_ZSH_HOME/plugins/asdf/plugins/perl.zsh
+source $XDG_ZSH_HOME/plugins/asdf/plugins/python.zsh
+source $XDG_ZSH_HOME/plugins/asdf/plugins/ruby.zsh
+source $XDG_ZSH_HOME/plugins/asdf/plugins/rust.zsh
 ##"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""##
