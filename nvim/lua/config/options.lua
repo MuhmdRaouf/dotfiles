92a6c152843e87ee3305b5731ed0702cd446ed81
Raouf <mohammed@raouf.me>
2024-08-03 21:18:53 +0300

    [VIM] update LazyVim configurations

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,11 +1,25 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
 
-vim.opt.swapfile = false
+-- global vim options
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 
+-- disable perl
 vim.g.loaded_perl_provider = 0
+
+-- LSP Server to use for PHP.
+-- Set to "intelephense" to use intelephense instead of phpactor.
+vim.g.lazyvim_php_lsp = "intelephense"
+
+-- LSP Server to use for Python.
+-- Set to "basedpyright" to use basedpyright instead of pyright.
+vim.g.lazyvim_python_lsp = "pyright"
+
+-- LSP Server to use for Ruby.
+-- Set to "solargraph" to use solargraph instead of ruby_lsp.
+vim.g.lazyvim_ruby_lsp = "solargraph"
+vim.g.lazyvim_ruby_formatter = "rubocop"
--- nvim/lua/plugins/nvim-tmux-navigation.lua
+++ nvim/lua/plugins/nvim-tmux-navigation.lua
@@ -1,7 +1,19 @@
-return { 'alexghergh/nvim-tmux-navigation', 
+return {
+  "alexghergh/nvim-tmux-navigation",
   config = function()
-    require('nvim-tmux-navigation').setup {
-      disable_when_zoomed = true -- defaults to false
+    require("nvim-tmux-navigation").setup({
+      disable_when_zoomed = true, -- defaults to false
+    })
+  end,
+  keys = function()
+    local nvim_tmux_navigation = require("nvim-tmux-navigation")
+    return {
+      { "<C-h>", nvim_tmux_navigation.NvimTmuxNavigateLeft },
+      { "<C-j>", nvim_tmux_navigation.NvimTmuxNavigateDown },
+      { "<C-k>", nvim_tmux_navigation.NvimTmuxNavigateUp },
+      { "<C-l>", nvim_tmux_navigation.NvimTmuxNavigateRight },
+      { "<C-\\>", nvim_tmux_navigation.NvimTmuxNavigateLastActive },
+      { "<C-Space>", nvim_tmux_navigation.NvimTmuxNavigateNext },
     }
   end,
 }
