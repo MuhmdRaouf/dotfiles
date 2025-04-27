29d037547270fdb4c4bb2527d4fe60f06fbd7e34
Raouf <mohammed@raouf.me>
2025-04-27 06:07:58 +0300

    [ZINIT] enhance zinit configs to work with intellij's terminal integration

--- zsh/zinit.zsh
+++ zsh/zinit.zsh
@@ -1,25 +1,29 @@
 ##"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""##
 #""                                   Zinit                                   ""#
 ##"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""##
-declare -A ZINIT  # initial Zinit's hash definition, if configuring before loading Zinit, and then:
-ZINIT[HOME_DIR]="$XDG_DATA_HOME/zinit"
-ZINIT[BIN_DIR]="${ZINIT[HOME_DIR]}/bin"
-ZINIT[PLUGIN_DIR]="${ZINIT[HOME_DIR]}/plugin"
-ZINIT[COMPLETIONS_DIR]="${ZINIT[HOME_DIR]}/completions"
-ZINIT[SNIPPETS_DIR]="${ZINIT[HOME_DIR]}/snippets"
-ZINIT[ZCOMPDUMP_PATH]="${ZINIT[HOME_DIR]}"
-ZINIT[COMPINIT_OPTS]="-C"
-# ZINIT[MUTE_WARNINGS]=1
-ZINIT[OPTIMIZE_OUT_DISK_ACCESSES]=1
-### Added by Zinit's installer
-if [[ ! -f ${ZINIT[HOME_DIR]}/bin/zinit.zsh ]]; then
-    print -P "%F{33}▓▒░ %F{220}Installing DHARMA Initiative Plugin Manager (zdharma/zinit)…%f"
-    command mkdir -p ${ZINIT[HOME_DIR]}
-    command git clone https://github.com/zdharma-continuum/zinit ${ZINIT[HOME_DIR]}/bin && \
+typeset -Ag ZINIT  
+typeset -gx ZINIT[MUTE_WARNINGS]=1
+typeset -gx ZINIT[COMPINIT_OPTS]="-C"
+typeset -gx ZINIT[OPTIMIZE_OUT_DISK_ACCESSES]=1
+typeset -gx ZINIT[HOME_DIR]="$XDG_DATA_HOME/zinit"
+typeset -gx ZINIT[BIN_DIR]="${ZINIT[HOME_DIR]}/bin"
+typeset -gx ZINIT[ZCOMPDUMP_PATH]="${ZINIT[HOME_DIR]}"
+typeset -gx ZINIT[PLUGIN_DIR]="${ZINIT[HOME_DIR]}/plugin"
+typeset -gx ZINIT[SNIPPETS_DIR]="${ZINIT[HOME_DIR]}/snippets"
+typeset -gx ZINIT[COMPLETIONS_DIR]="${ZINIT[HOME_DIR]}/completions"
+
+if [[ ! -f "${ZINIT[HOME_DIR]}/bin/zinit.zsh" ]]; then
+    print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
+    command mkdir -p "${ZINIT[HOME_DIR]}"
+    command git clone --depth=1 https://github.com/zdharma-continuum/zinit "${ZINIT[HOME_DIR]}/bin" && \
         print -P "%F{33}▓▒░ %F{34}Installation successful.%f" || \
         print -P "%F{160}▓▒░ The clone has failed.%f"
 fi
+
 source "${ZINIT[HOME_DIR]}/bin/zinit.zsh"
 autoload -Uz _zinit
-(( ${+_comps} )) && _comps[zinit]=_zinit
+
+if (( ${+_comps} )); then
+  _comps[zinit]=_zinit
+fi
 ##"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""##
