a2b68e0a9c0ebef5048789b107813b88d0dc9578
Raouf <mohammed@raouf.me>
2026-02-17 09:17:14 +0200

    [MISE] update configuration file

--- zsh/plugins/mise/plugins/node.zsh
+++ zsh/plugins/mise/plugins/node.zsh
@@ -4,9 +4,7 @@
 export MISE_NODE_COREPACK=true
 export MISE_NODE_DEFAULT_PACKAGES_FILE="$MISE_CONFIG_DIR/default-node-packages"
 [[ -e "$MISE_NODE_DEFAULT_PACKAGES_FILE" ]] || cat >> "$MISE_NODE_DEFAULT_PACKAGES_FILE" << EOM
-@anthropic-ai/claude-code
 @google/gemini-cli
-@musistudio/claude-code-router
 gitlab-ci-local
 npm
 npm-check-updates
